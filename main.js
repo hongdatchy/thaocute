a4f374625a805344607f21271a1e76bd2b7df047
datph6 <datph6@fpt.com.vn>
2023-10-20 00:05:29 +0700

    s

---
+++ main.js
@@ -0,0 +1,12 @@
+setTimeout(()=>{
+    document.getElementById("bg").classList.remove("bg")
+}, 1000)
+
+setTimeout(()=>{
+    document.getElementById("thao").classList.add("xoay")
+}, 3000)
+
+
+setTimeout(()=>{
+    document.getElementsByClassName("sologan")[0].classList.add("xuong")
+}, 6000)
